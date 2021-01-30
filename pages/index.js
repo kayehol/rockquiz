f9e3b590aa319bb7b640e4a32470cdecd76a5bb6
kayehol <kaye.holanda@gmail.com>
2021-01-30 19:49:31 -0300

    aula 04

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
+/* eslint-disable semi */
 import React from 'react'
-import styled from "styled-components";
-import db from "../db.json";
-import Widget from "../src/components/Widget";
-import QuizLogo from "../src/components/QuizLogo";
-import QuizBackground from "../src/components/QuizBackground";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
-import Head from 'next/head'
-import { useRouter } from 'next/router'
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input'
+import Button from '../src/components/Button'
 
 /*
 const BackgroundImage = styled.div`
@@ -23,7 +26,7 @@ const ButtonStyle = styled.div`
   padding-top: 1em;
   display: flex;
   justify-content: center;
-`
+`;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -37,55 +40,70 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  const router = useRouter()
-  const [name, setName] = React.useState('')
+  const router = useRouter();
+  const [name, setName] = React.useState('');
 
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
         <title>Rock Quiz</title>
         <meta name="title" content="Rock Quiz" />
-        <meta name="description" content="Teste os seus conhecimentos sobre bandas de rock e vamos ver quantas caixas de som você vai quebrar."/>
+        <meta
+          name="description"
+          content="Teste os seus conhecimentos sobre bandas de rock e vamos ver quantas caixas de som você vai quebrar."
+        />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://rockquiz.vercel.app" />
         <meta property="og:title" content="Rock Quiz" />
-        <meta property="og:description" content="Teste os seus conhecimentos sobre bandas de rock e vamos ver quantas caixas de som você vai quebrar."/>
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        <meta
+          property="og:description"
+          content="Teste os seus conhecimentos sobre bandas de rock e vamos ver quantas caixas de som você vai quebrar."
+        />
+        <meta
+          property="og:image"
+          content="http://authorityalchemy.com/wp-content/uploads/2015/11/sex-pistols-5041ff2ab1b3c.jpg"
+        />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:url" content="https://rockquiz.vercel.app/" />
         <meta property="twitter:title" content="Rock Quiz" />
-        <meta property="twitter:description" content="Teste os seus conhecimentos sobre bandas de rock e vamos ver quantas caixas de som você vai quebrar."/>
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"/>
+        <meta
+          property="twitter:description"
+          content="Teste os seus conhecimentos sobre bandas de rock e vamos ver quantas caixas de som você vai quebrar."
+        />
+        <meta
+          property="twitter:image"
+          content="http://authorityalchemy.com/wp-content/uploads/2015/11/sex-pistols-5041ff2ab1b3c.jpg"
+        />
       </Head>
       <QuizContainer>
         <QuizLogo />
         <Widget>
-            <Widget.Header>
-              <h1>{db.title}</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <form onSubmit={function(infosDoEvento){
-                  infosDoEvento.preventDefault()
-                  router.push(`/quiz?name=${name}`)
-                  console.log('Fazendo uma submissão por meio do react')
-                }}>
-                <input 
-                  onChange={function(infosDoEvento) {
-                    console.log(infosDoEvento.target.value)
-                    //state
-                    setName(infosDoEvento.target.value)
-                  }} 
-                  placeholder='Diz aí seu nome' 
-                />
-                <ButtonStyle>
-                  <button type='submit' disable={name.length === 0}>
-                    Jogar {name}
-                  </button>
-                </ButtonStyle>
-              </form>
-            </Widget.Content>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <form
+              onSubmit={(infosDoEvento) => {
+                infosDoEvento.preventDefault();
+                router.push(`/quiz?name=${name}`);
+                console.log('Fazendo uma submissão por meio do react');
+              }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz aí seu nome"
+                value={name}
+              />
+              <ButtonStyle>
+                <Button type="submit" disable={name.length === 0}>
+                  Jogar {name}
+                </Button>
+              </ButtonStyle>
+            </form>
+          </Widget.Content>
         </Widget>
         <Widget>
           <Widget.Header>
@@ -97,7 +115,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/kayehol" />
+      <GitHubCorner projectUrl="https://github.com/kayehol/rockquiz" />
     </QuizBackground>
   );
 }
